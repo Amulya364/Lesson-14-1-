ca6d1fe5d32f8cfb64100e34b9247af793d8688b
Amulya Manoj <amulyamanoj@berkeley.edu>
2025-05-25 12:42:20 -0700

    Initial Commit

---
+++ Lesson 14/Lesson_14App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_14App.swift
+//  Lesson 14
+//
+//  Created by Scholar on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lesson 14Tests/Lesson_14Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_14Tests.swift
+//  Lesson 14Tests
+//
+//  Created by Scholar on 5/25/25.
+//
+
+import Testing
+@testable import Lesson_14
+
+struct Lesson_14Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
